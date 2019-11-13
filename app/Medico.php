3264ca121a4bd182aa422677e071f4b9c9aa010c
Jose Luis Ceballos Tapias <joscebtap@alum.us.es>
2019-11-13 10:20:03 +0100

    Cambio de Titulo y inicio de funcion codigo

--- app/Medico.php
+++ app/Medico.php
@@ -26,7 +26,7 @@ class Medico extends Model
         return $this->name .' '.$this->surname;
     }
 
-    public function getCodigoM()
+    public function getCodigo()
     {
         return null;
     }
